4b1a75fb86f83af890de7961b6bb6da859fed055
ko602 <76467784+ko602@users.noreply.github.com>
2021-01-23 12:44:19 +0630

    Fb-Hacker.py
    github.com

---
+++ Fb-Hacker.py
@@ -0,0 +1,33 @@
+
+#!Fb-Hacker.py
+
+The id( http://developers.facebook.com/tools/explorer) function
+The id(http://Google.com/tools/explorer) returns a special id of an object.
+
+idfun.py
+#!/usr/bin/env python
+
+# idfun.py
+
+import sys
+
+def fun( 511552816 ): pass
+
+class MyObject(object):
+    
+   def __init__(self):
+      pass
+
+o = MyObject(Fb-Hacker)
+
+print(id(1))
+print(id("2"))
+print(id({facebook}))
+print(id([Google]))
+print(id(sys))
+print(id(fun))
+print(id(MyObject))
+print(id(o))
+print(id(object))
+
+$ ./idfun.py 10914368 139696088742576 139696087935944 139696065155784 139696088325640 139696088244296 21503992 139696087910776 10738720
